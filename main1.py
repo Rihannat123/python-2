89426c54456666c612cf38bd4f8385ccf0d17337
Rihannat123 <rihannat.oguns@gmail.com>
2025-08-30 18:46:33 +0100

    python

---
+++ main1.py
@@ -0,0 +1,9 @@
+# input a word
+text=str(input("enter a string"))
+
+#Reverse a string\
+revText=text[::-1]
+text=revText
+
+#printing the reverse string
+print("Reverse of the give string is :", text)
